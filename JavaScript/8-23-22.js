4a195b8b55b91990d37fc81066acd7af6e5d7f37
Aiden Luce <luceaiden4124@gmail.com>
2022-08-23 15:35:31 -0500

    Comparison Oporators Practice

--- JavaScript/8-23-22.js
+++ JavaScript/8-23-22.js
@@ -40,7 +40,40 @@
 // console.log(`Divide y: ${x /= y}`)
 
 /** Comparison Operators */
-//
+// let x = 12
+// function check1(input){
+//     return (input === 12)
+// }
+// function check2(input){
+//     return (input == '12')
+// }
+// function check3(input){
+//     return (input === '12')
+// }
+// function check4(input){
+//     return (input !== 12)
+// }
+// function check5(input){
+//     return (input === true)
+// }
+// function check6(input){
+//     return (input === false)
+// }
+// function check7(input){
+//     return (input === NaN)
+// }
+// function check8(input){
+//     return (input === null)
+// }
+// console.log(check1(x))
+// console.log(check2(x))
+// console.log(check3(x))
+// console.log(check4(x))
+// console.log(check5(x))
+// console.log(check6(x))
+// console.log(check7(x))
+// console.log(check8(x))
+
 /** String Manipulation */
 //
 /** Array Manipulation */
