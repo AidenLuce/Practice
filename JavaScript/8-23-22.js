74020f426683445518c0ddcfa17e0c3b62a59175
Aiden Luce <luceaiden4124@gmail.com>
2022-08-23 15:08:16 -0500

    Variables Practice completed

--- JavaScript/8-23-22.js
+++ JavaScript/8-23-22.js
@@ -1,4 +1,4 @@
-/** */
+/** Values Practice */
 // function boolToWord( bool ){
 //     if(bool === true){
 //         return('Yes')
@@ -6,5 +6,37 @@
 //         return('No')
 //     }
 // }
-/** */
+/** Variables Practice */
+// let a = 1
+// let b = 2
+// let c = 3
+// let d = 4
+// function addVariables(){
+//     return (a + b + c + d)
+// }
+// console.log(addVariables())
+
+/** Data Types */
+// function checkIfBoolean(input){
+//     if(typeof input === 'number'){
+//         return (`${input} is not a boolean.`);
+//     }else if (typeof input === 'undefined'){
+//         return (`${input} is not a boolean.`);
+//     }else if (typeof input === 'string'){
+//         return (`${input} is not a boolean.`);
+//     }else if (input === null){
+//         return (`${input} is not a boolean.`);
+//     }else if (typeof input === 'boolean' ){
+//         return (`${input} is a boolean!`);
+//     }
+// }
+/** Assignment Operators */
+//
+/** Comparison Operators */
+//
+/** String Manipulation */
+//
+/** Array Manipulation */
+//
+/** Writing Functions */
 //
